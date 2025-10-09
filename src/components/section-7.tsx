15cd0b2c46667e14680cde604042b30d1f5be2bf
Pattapon Vichanukroh <Pattapon.V@Pattapons-MacBook-Pro.local>
2025-10-10 00:09:52 +0700

    feat: update margin top

--- src/components/section-7.tsx
+++ src/components/section-7.tsx
@@ -2,7 +2,7 @@ import CtaText from "./ui/cta"
 
 const Section7 = () => {
     return (
-        <section className="w-full py-12 md:py-24 lg:py-32 bg-main-indigo text-white">
+        <section className="w-full pt-20 md:pt-0 py-12 md:py-24 lg:py-32 bg-main-indigo text-white">
             <div className="container mx-auto px-4">
                 <CtaText />
             </div>
